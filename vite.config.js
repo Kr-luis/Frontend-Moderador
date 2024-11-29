58f0e9aa01f4d5821079d15940c312ac43b62e2e
Eduardo-Caza <ecaanaranjo@gmail.com>
2024-11-29 00:09:57 -0500

    arreglando vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Escucha en todas las interfaces de red
+    port: process.env.PORT || 3000, // Utiliza el puerto proporcionado por Render o el puerto 3000
+  },
 })
