41e9c7b04a056988512901565290e4fd3e4795fc
Eduardo-Caza <ecaanaranjo@gmail.com>
2024-11-29 00:58:21 -0500

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { DarkModeContextProvider } from './context/darkModeContext'
-// import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { DarkModeContextProvider } from './context/darkModeContext';
+// import './index.css'; // Descomenta esto si necesitas aplicar estilos globales
 
+// Asegúrate de que el contexto y el componente `App` estén correctamente importados y configurados.
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <DarkModeContextProvider>
-        <App />
+      <App />
     </DarkModeContextProvider>
+  </React.StrictMode>
 );
